537c2695de16b3f50d4f25c8768b8cc192835730
Devanshi24122001 <120907024+Devanshi24122001@users.noreply.github.com>
2023-02-20 11:31:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomDiceImage = "dice" + randomNumber1 + ".png";
+var randomImageSources = "images/" + randomDiceImage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src", randomImageSources);
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+var randomImageSources2 = "images/dice" + randomNumber2 + ".png";
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSources2);
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "Player 1 wins!";
+}
+else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins!";
+}
+else {
+    document.querySelector("h1").innerHTML = "Draw!"
+}
